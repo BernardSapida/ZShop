96055fbea03bd268d12f6a8c177e97038c66ebda
BernardSapida <BernardSapida1706@gmail.com>
2023-03-14 12:54:40 +0800

    testing for production

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,52 @@
 import EventCard from "@/components/EventCard";
 import fs from "fs/promises";
 import path from "path";
+import useSWR from "swr";
+import { useEffect, useState } from "react";
+
+const fetcher = async (url: string) => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+};
 
 export async function getStaticProps() {
   const filePath = path.join(process.cwd(), "data/EventList.json");
   const jsonData = await fs.readFile(filePath, "utf-8");
   const EventList = JSON.parse(jsonData).events;
 
+  const data = await fetcher(
+    "https://sampledeno-default-rtdb.firebaseio.com/data.json"
+  );
+
   return {
     props: {
       list: [...EventList],
+      data: data,
     },
-    revalidate: 10,
-    notFound: false,
+    // revalidate: 10,
   };
 }
 
 export default function Home(props: any) {
+  const keys = Object.keys(props.data);
+  const [names, setNames] = useState(props.data);
+  const { data, error } = useSWR(
+    "https://sampledeno-default-rtdb.firebaseio.com/data.json",
+    fetcher
+  );
+
+  useEffect(() => {
+    if (!data) console.log("Loading...");
+    else setNames(data);
+  }, [data, error]);
+
   return (
     <>
+      {keys.map((key: any) => {
+        return <p key={key}>{names[key]["name"]}</p>;
+      })}
       {props.list.map((event: Record<string, any>) => (
         <EventCard
           key={event.id}
