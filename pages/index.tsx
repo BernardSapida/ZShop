de63d3156ad081270a6eeb52dc116bc0d60074cf
BernardSapida <BernardSapida1706@gmail.com>
2023-03-14 16:21:25 +0800

    UPDATE

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,8 @@ export default function Home(props: any) {
   const [names, setNames] = useState(props.data);
   const { data, error } = useSWR(
     "https://sampledeno-default-rtdb.firebaseio.com/data.json",
-    fetcher
+    fetcher,
+    { refreshInterval: 1000 }
   );
 
   useEffect(() => {
